a994d077c69f7ca81eeed07642af83fc742cc96c
Francisco Coelho <franciscocoelho.ti@gmail.com>
2026-09-21 20:08:25 -0300

    Criação dos diretórios de templates, tanto no app Gestão como o diretório base na raiz do projeto

--- django/gestao_projetos/gestao/urls.py
+++ django/gestao_projetos/gestao/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import gestao_home
 
 urlpatterns = [
-    path('', gestao_home)
+    path('', gestao_home),
+    # path('projetos', listar_projetos())
 ]
--- django/gestao_projetos/gestao/views.py
+++ django/gestao_projetos/gestao/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from .models import Projeto
 
 def gestao_home(request):
-    return render(request, 'index.html')
+    return render(request, 'gestao/index.html')
+
+def listar_projetos(request):
+    projetos = Projeto.objects.all()
+    return render(
+        request,
+        'gestao/projetos.html',
+        {'context' : projetos}
+    )
 
